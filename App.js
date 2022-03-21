6d4494d639ac4e43da326ef513a3dd0725639e5c
BT278 <64267917+BT278@users.noreply.github.com>
2022-03-21 18:58:31 -0500

    updated copy 3-21-22

--- App.js
+++ App.js
@@ -1,20 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component, useState } from "react";
+import { StyleSheet, Button, Text, View, Image, Dimensions, TouchableOpacity, Touchable, LogBox } from "react-native";
+import MapView, { PROVIDER_GOOGLE, Marker, Callout } from "react-native-maps";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import uhclStyle from './mapStyle';
+
+const deltaImage = require("./img/delta-building.jpg");
+const bayouImage = require("./img/bayou-building.jpg");
+
+// Ignore log notification by message:
+LogBox.ignoreLogs(['Remote debugger']);
+
+export default class hawkExpressTracker extends Component {
+  render() {
+    return (
+      
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        style={styles.map}
+        customMapStyle={uhclStyle} // custom style from mapstyles.js
+        ref={(ref) => {
+          this.map = ref;
+        }}
+        enableZoomControl={true}
+        showsMyLocationButton={true}
+        initialRegion={region}
+      >
+
+        <Marker coordinate={{ latitude: 29.578252, longitude: -95.104159 }}>
+          <Callout>
+            <Image source={require("./img/delta-building.jpg")} />
+            <Text>Delta Building</Text>
+          </Callout>
+        </Marker>
+
+        <Marker 
+          coordinate={{ latitude: 29.582925, longitude: -95.097646 }}>
+          <Callout>
+            <Image source={require("./img/bayou-building.jpg")} />
+            <Text>Bayou Student Entrance (East)</Text>
+          </Callout>
+        </Marker>
+
+      </MapView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 40,
+  },
+  map: {
+    height: "70%",
   },
 });
+
+var region = {
+  latitude: 29.584,
+  longitude: -95.102,
+  latitudeDelta: 0.015,
+  longitudeDelta: 0.015,
+};
