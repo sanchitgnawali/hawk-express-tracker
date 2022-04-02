32749113c1b493ac8ff526f33ae37c47ad7f3e80
Jeffery Rubio <rubioj3781@uhcl.edu>
2022-04-02 11:32:30 -0500

    updated login_signup_style

---
+++ screens/Signup.js
@@ -0,0 +1,47 @@
+import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity, ScrollView} from 'react-native'
+import React, {useState} from 'react'
+import {styleSheet} from "../styles/accountStyle"
+
+const SignUp = ({navigation}) => {
+  return (
+    <ScrollView contentContainerStyle={styleSheet.container}>
+        <Image source={require('../assets/hawkExpressLogo.png')} style={styleSheet.image}/>
+
+        <View style={styleSheet.titleContainer}>
+        <Text style={styleSheet.titleStyle}>Hawk Express Tracker</Text>
+        <Text style={styleSheet.subtitleLabel}>Create new account</Text>
+        </View>
+
+        <View style={styleSheet.inputContainer}>
+            <Text style={styleSheet.inputLabel}>Name</Text>
+            <TextInput placeholder='Bob Smith' style={styleSheet.input}/>
+
+            <Text style={styleSheet.inputLabel}>Email</Text>
+            <TextInput placeholder="bsmith1234@uhcl.edu" style={styleSheet.input}/>
+            
+            <Text style={styleSheet.inputLabel}>Password</Text>
+            <TextInput placeholder='********' style={styleSheet.input} secureTextEntry/>
+            
+            <Text style={styleSheet.inputLabel}>Confirm Password</Text>
+            <TextInput placeholder='********' style={styleSheet.input} secureTextEntry/>
+        </View>
+        
+        <View style={styleSheet.buttonContainer}>
+            <TouchableOpacity onPress={()=>navigation.navigate('LoginPage')} style={styleSheet.button}>
+                <Text style={styleSheet.buttonText}>Create Account</Text>
+            </TouchableOpacity> 
+        </View>
+
+        <Text style={styleSheet.subtitleStyle}>Already have an account? {' '}
+          <Text
+            style={{color:'#0078AE'}}
+            onPress={()=>navigation.navigate('LoginPage')}>
+            Login
+          </Text>
+        </Text>
+
+    </ScrollView>
+  )
+}
+
+export default SignUp
