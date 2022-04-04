09c08d2e8ee2701bd49547dbf0f72b94cb0e06ad
Sanchit Gnawali <sanchitgnawali@gmail.com>
2022-04-04 12:18:57 -0500

    created AuthContext for global scope and signup hook to perform signup

---
+++ context/AuthContext.js
@@ -0,0 +1,9 @@
+import { createContext, useEffect, useReducer } from "react";
+import { auth } from "../firebase";
+
+//creating a context
+export const AuthContext = createContext();
+
+export const AuthContextProvider = ({ children }) => {
+  // to do
+};
--- src/screens/Signup.js
+++ src/screens/Signup.js
@@ -1,86 +1,97 @@
-import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity, ScrollView} from 'react-native'
-import React, {useState, useContext} from 'react'
-import {styleSheet} from "../styles/accountStyle"
-import { auth } from '../../firebase'
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
+import React, { useState, useContext } from "react";
+import { styleSheet } from "../styles/accountStyle";
+import { auth } from "../../firebase";
 
-const SignUp = ({navigation}) => {
-  
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [name, setName] = useState('')
+const SignUp = ({ navigation }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
 
   const handleSignup = () => {
-    auth 
+    auth
       .createUserWithEmailAndPassword(email, password)
       .then(navigation.navigate("Login"))
-     // .then(userCredentials => {
-     //   const user = userCredentials.user;
+      // .then(userCredentials => {
+      //   const user = userCredentials.user;
       //  console.log(user.email);
-     // })
-      .catch(error => alert(error.message))
-  }
-  
+      // })
+      .catch((error) => alert(error.message));
+  };
+
   return (
     <ScrollView contentContainerStyle={styleSheet.container}>
-        <Image source={require('../images/hawkExpressLogo.png')} style={styleSheet.image}/>
+      <Image
+        source={require("../images/hawkExpressLogo.png")}
+        style={styleSheet.image}
+      />
 
-        <View style={styleSheet.titleContainer}>
+      <View style={styleSheet.titleContainer}>
         <Text style={styleSheet.titleStyle}>Hawk Express Tracker</Text>
         <Text style={styleSheet.subtitleLabel}>Create new account</Text>
-        </View>
+      </View>
 
-        <View style={styleSheet.inputContainer}>
-            <Text style={styleSheet.inputLabel}>Name</Text>
-            <TextInput 
-              placeholder='Bob Smith' 
-              value={name}
-              onChangeText={text => setName(text)}
-              style={styleSheet.input}
-              />
+      <View style={styleSheet.inputContainer}>
+        <Text style={styleSheet.inputLabel}>Name</Text>
+        <TextInput
+          placeholder="Bob Smith"
+          value={name}
+          onChangeText={(text) => setName(text)}
+          style={styleSheet.input}
+        />
 
-            <Text style={styleSheet.inputLabel}>Email</Text>
-            <TextInput 
-              placeholder="bsmith1234@uhcl.edu" 
-              value={email}
-              onChangeText={text => setEmail(text)}
-              style={styleSheet.input}/>
-            
-            <Text style={styleSheet.inputLabel}>Password</Text>
-            <TextInput 
-              placeholder='********' 
-              value={password}
-              onChangeText={text => setPassword(text)}
-              style={styleSheet.input} 
-              secureTextEntry/>
-            
-            <Text style={styleSheet.inputLabel}>Confirm Password</Text>
-            <TextInput 
-              placeholder='********' 
-              value={password}
-              onChangeText={text => setPassword(text)}
-              style={styleSheet.input} 
-              secureTextEntry/>
-        </View>
-        
-        <View style={styleSheet.buttonContainer}>
-            <TouchableOpacity 
-              onPress={handleSignup}
-              style={styleSheet.button}
-            >
-              <Text style={styleSheet.buttonText}>Create Account</Text>
-            </TouchableOpacity> 
-        </View>
+        <Text style={styleSheet.inputLabel}>Email</Text>
+        <TextInput
+          placeholder="bsmith1234@uhcl.edu"
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+          style={styleSheet.input}
+        />
 
-        <Text style={styleSheet.subtitleStyle}>Already have an account? {' '}
-          <Text
-            style={{color:'#0078AE'}}
-            onPress={()=>navigation.navigate('Login')}>
-            Login
-          </Text>
+        <Text style={styleSheet.inputLabel}>Password</Text>
+        <TextInput
+          placeholder="********"
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+          style={styleSheet.input}
+          secureTextEntry
+        />
+
+        <Text style={styleSheet.inputLabel}>Confirm Password</Text>
+        <TextInput
+          placeholder="********"
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+          style={styleSheet.input}
+          secureTextEntry
+        />
+      </View>
+
+      <View style={styleSheet.buttonContainer}>
+        <TouchableOpacity onPress={handleSignup} style={styleSheet.button}>
+          <Text style={styleSheet.buttonText}>Create Account</Text>
+        </TouchableOpacity>
+      </View>
+
+      <Text style={styleSheet.subtitleStyle}>
+        Already have an account?{" "}
+        <Text
+          style={{ color: "#0078AE" }}
+          onPress={() => navigation.navigate("Login")}
+        >
+          Login
         </Text>
-
+      </Text>
     </ScrollView>
-  )
-}
+  );
+};
 
-export default SignUp
+export default SignUp;
