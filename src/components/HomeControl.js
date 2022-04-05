0218644fb1a9b7349b395821e003b80898d86506
Sanchit Gnawali <sanchitgnawali@gmail.com>
2022-04-04 21:30:35 -0500

    Created Profile Screen and setup simple authentication

---
+++ src/components/HomeControl.js
@@ -0,0 +1,36 @@
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+
+export default HomeControl = () => {
+  return (
+    <View style={styles.homeControl}>
+      <Text style={styles.text}>Ready To Go?</Text>
+
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Select Destination</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  homeControl: {
+    height: "15%",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 10,
+  },
+  text: {
+    fontSize: 20,
+  },
+  button: {
+    marginLeft: 10,
+    padding: 10,
+    backgroundColor: "#0782F9",
+    borderRadius: 10,
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 16,
+  },
+});
---
+++ src/screens/Profile.js
@@ -0,0 +1,41 @@
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { auth } from "../../firebase";
+
+export default Profile = ({ navigation }) => {
+  const handleSignout = () => {
+    auth.signOut().catch((error) => alert(error.message));
+  };
+
+  return (
+    <View style={styles.profileContainer}>
+      <Text style={styles.text}>Welcome to profile</Text>
+
+      <TouchableOpacity onPress={handleSignout} style={styles.button}>
+        <Text style={styles.buttonText}>Sign Out</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  profileContainer: {
+    flex: 1,
+    alignSelf: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 30,
+  },
+  button: {
+    backgroundColor: "#0782F9",
+    width: "25%",
+    padding: 10,
+    borderRadius: 10,
+  },
+  buttonText: {
+    color: "white",
+    alignItems: "center",
+    fontWeight: "700",
+    fontSize: 16,
+  },
+});
