4cb3cb31da9b4ed3cd62e7a1b0bf7dcf1476522f
BT278 <myerlys5263@uhcl.edu>
2022-04-05 23:10:56 -0500

    Crappy code, but it adds in waypoints on the map

--- src/components/MapComponent.js
+++ src/components/MapComponent.js
@@ -1,8 +1,16 @@
 import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
 import MapView, { PROVIDER_GOOGLE, Marker, Callout } from "react-native-maps";
+import MapViewDirections from "react-native-maps-directions";
 import { auth } from "./../../firebase";
 //Files
 import uhclStyle from "../styles/mapStyle";
+
+const handleSignout = () => {
+  auth
+    .signOut()
+    // need to get navigation back to login working for here
+    .catch((error) => alert(error.message));
+};
 
 var region = {
   latitude: 29.584,
@@ -11,12 +19,60 @@ var region = {
   longitudeDelta: 0.015,
 };
 
-const handleSignout = () => {
-  auth
-    .signOut()
-    // need to get navigation back to login working for here
-    .catch((error) => alert(error.message));
-};
+const places = [
+  {
+    latitude: 29.561950133,
+    longitude: -95.115615515,
+  },
+
+  {
+    latitude: 29.552874933427443,
+    longitude: -95.12420096370195,
+  },
+
+  {
+    latitude: 29.569863566531023,
+    longitude: -95.11018505215024
+  },
+
+  { 
+    latitude: 29.574083302941812, 
+    longitude: -95.10754740123505 
+  },
+
+  { 
+    latitude: 29.57689945321822, 
+    longitude: -95.10630695303462 
+  },
+
+  { 
+    latitude: 29.578042477429793, 
+    longitude: -95.1028376873912 
+  },
+
+  { 
+    latitude: 29.58665434643349, 
+    longitude: -95.0949528817226 
+  },
+
+  { latitude: 29.585956169582044, 
+    longitude: -95.09822634535767 
+  },
+
+  { latitude: 29.58533148291235, 
+    longitude: -95.09792277159933 
+  },
+
+  { 
+    latitude: 29.584092478872343, 
+    longitude: -95.09768513897552 
+  },
+
+  { 
+    latitude: 29.582925, 
+    longitude: -95.097646 
+  },
+];
 
 // const imageWidth = Dimensions.get("window").width;
 
@@ -30,8 +86,103 @@ export default MapComponent = () => {
         initialRegion={region}
         showUserLocation
       >
+        <MapViewDirections
+          origin={places[0]}
+          destination={places[1]}
+          apikey={"INSERT KEY"} // insert your API Key here
+          strokeWidth={4}
+          strokeColor="#0078AE"
+          precision="high"
+        />
+        <MapViewDirections
+          origin={places[1]}
+          destination={places[2]}
+          apikey={"INSERT KEY"} // insert your API Key here
+          strokeWidth={4}
+          strokeColor="#0078AE"
+          precision="high"
+        />
+        <MapViewDirections
+          origin={places[2]}
+          destination={places[3]}
+          apikey={"INSERT KEY"} // insert your API Key here
+          strokeWidth={4}
+          strokeColor="#0078AE"
+          precision="high"
+        />
+        <MapViewDirections
+          origin={places[3]}
+          destination={places[4]}
+          apikey={"INSERT KEY"} // insert your API Key here
+          strokeWidth={4}
+          strokeColor="#0078AE"
+          precision="high"
+        />
+        <MapViewDirections
+          origin={places[4]}
+          destination={places[5]}
+          apikey={"INSERT KEY"} // insert your API Key here
+          strokeWidth={4}
+          strokeColor="#0078AE"
+          precision="high"
+        />
+        <MapViewDirections
+          origin={places[5]}
+          destination={places[6]}
+          apikey={"INSERT KEY"} // insert your API Key here
+          strokeWidth={4}
+          strokeColor="#0078AE"
+          precision="high"
+        />
+        <MapViewDirections
+          origin={places[6]}
+          destination={places[7]}
+          apikey={"INSERT KEY"} // insert your API Key here
+          strokeWidth={4}
+          strokeColor="#0078AE"
+          precision="high"
+        />
+        <MapViewDirections
+          origin={places[7]}
+          destination={places[8]}
+          apikey={"INSERT KEY"} // insert your API Key here
+          strokeWidth={4}
+          strokeColor="#0078AE"
+          precision="high"
+        />
+        <MapViewDirections
+          origin={places[8]}
+          destination={places[9]}
+          apikey={"INSERT KEY"} // insert your API Key here
+          strokeWidth={4}
+          strokeColor="#0078AE"
+          precision="high"
+        />
+        <MapViewDirections
+          origin={places[9]}
+          destination={places[10]}
+          apikey={"INSERT KEY"} // insert your API Key here
+          strokeWidth={4}
+          strokeColor="#0078AE"
+          precision="high"
+        />
+        <MapViewDirections
+          origin={places[10]}
+          destination={places[11]}
+          apikey={"INSERT KEY"} // insert your API Key here
+          strokeWidth={4}
+          strokeColor="#0078AE"
+          precision="high"
+        />
+
+
+
+
         <Marker
-          coordinate={{ latitude: 29.561950133, longitude: -95.115615515 }}
+          coordinate={{
+            latitude: 29.561950133,
+            longitude: -95.115615515,
+          }}
         >
           <Callout>
             <Text>Christ's Church</Text>
