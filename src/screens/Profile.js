673b641dc38247bb83c5180b4cd08dad94f5f91c
Sean Myerly <SMyerly27@gmail.com>
2022-04-23 10:13:58 -0500

    Merged older format with the new password update

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,48 +1,103 @@
-import { Text, View, Image, TextInput, TouchableOpacity, ScrollView, StyleSheet } from 'react-native';
+import {
+  Text,
+  View,
+  Image,
+  TextInput,
+  TouchableOpacity,
+  ScrollView,
+  StyleSheet,
+} from "react-native";
 import { auth } from "../../firebase";
-import {useState} from 'react';
-import {styleSheet} from "../styles/accountStyle";
+import { useState, useEffect } from "react";
+import { styleSheet } from "../styles/accountStyle";
 import { useLogout } from "../../hooks/useLogout";
-import React from 'react';
+import React from "react";
 
 export default Profile = ({ navigation }) => {
   const { logout } = useLogout();
 
-  const [password1, setPassword1] = useState('');
-  const [password2, setPassword2] = useState('');
+  const [password1, setPassword1] = useState("");
+  const [password2, setPassword2] = useState("");
+  const [userEmail, setUserEmail] = useState("");
+  const [userName, setUserName] = useState("");
 
-  const handleSignout = () => {
-    logout();
-    // auth.signOut().catch((error) => alert(error.message));
+  const updatePassword = () => {
+    if (!password1 && !password2) {
+      alert("Passwords can't be left blank.");
+    } else if (password1 === password2) {
+      const user = auth.currentUser;
+
+      user
+        .updatePassword(password1)
+        .then(() => {
+          alert("Password updated.");
+        })
+        .catch((error) => {
+          alert(error.message);
+        });
+    } else {
+      alert("Passwords you entered don't match. Please retype and try again.");
+    }
   };
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user) {
+        setUserEmail(user.email);
+        setUserName(user.displayName);
+      }
+    });
+
+    return unsubscribe;
+  }, []);
 
   return (
     <ScrollView contentContainerStyle={styleSheet.container}>
-        <Image source={require('../images/hawk-logo.png')} style={styles.image}/>
-            <View style={styleSheet.titleContainer}>
-              <Text style={styleSheet.titleStyle}>Edit Profile</Text>
-              <Text style={styleSheet.subtitleLabel}>Enter new information and click "Confirm Profile Updates". Leave boxes blank to keep information the same.</Text>
-            </View>
+      <Image source={require("../images/hawk-logo.png")} style={styles.image} />
+      <View style={styleSheet.titleContainer}>
+        <Text style={styleSheet.titleStyle}>Welcome, {userName} </Text>
+        <Text style={styleSheet.subtitleLabel}>Enter new information and click "Confirm Profile Updates". Leave boxes blank to keep information the same.
+        </Text>
+      </View>
 
       <View style={styleSheet.inputContainer}>
-         <Text style={styleSheet.inputLabel}>Email</Text>
-         <TextInput placeholder="bsmith1234@uhcl.edu" editable = {true} style={styleSheet.input}/>
+        <Text style={styleSheet.inputLabel}>Email</Text>
+        <TextInput
+          placeholder="bsmith1234@uhcl.edu"
+          editable={false}
+          style={styleSheet.input}
+          value={userEmail}
+        />
 
-         <Text style={styleSheet.inputLabel}>Change Password</Text>
-         <TextInput placeholder='********' onChange={(e) => setPassword1(e.target.value)} style={styleSheet.input} secureTextEntry/>
+        <Text style={styleSheet.inputLabel}>Change Password</Text>
+        <TextInput
+          placeholder="********"
+          value={password1}
+          onChangeText={(text) => setPassword1(text)}
+          style={styleSheet.input}
+          secureTextEntry
+        />
 
-         <Text style={styleSheet.inputLabel}>Re-Enter New Password</Text>
-         <TextInput placeholder='********' onChange={(e) => setPassword2(e.target.value)} style={styleSheet.input} secureTextEntry/>
+        <Text style={styleSheet.inputLabel}>Re-Enter New Password</Text>
+        <TextInput
+          placeholder="********"
+          value={password2}
+          onChangeText={(text) => setPassword2(text)}
+          style={styleSheet.input}
+          secureTextEntry
+        />
       </View>
-      
-      <View style={styleSheet.buttonContainer}>
 
-      <TouchableOpacity onPress={()=>{if(password1==password2){console.log("Doesn't work because we're dumb")}}} style={styles.profileButton}>
-            <Text style={styleSheet.buttonText}>Confirm Profile Updates</Text>
-            </TouchableOpacity>
-      <TouchableOpacity onPress={() => navigation.navigate("HomeScreen")} style={styles.profileButton}>
-        <Text style={styleSheet.buttonText}>Return to Map</Text>
-      </TouchableOpacity>
+      <View style={styleSheet.buttonContainer}>
+        <TouchableOpacity onPress={updatePassword} style={styles.profileButton}>
+          <Text style={styleSheet.buttonText}>Confirm Profile Updates</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => navigation.navigate("HomeScreen")}
+          style={styles.profileButton}
+        >
+          <Text style={styleSheet.buttonText}>Return to Map</Text>
+        </TouchableOpacity>
       </View>
     </ScrollView>
   );
@@ -50,9 +105,10 @@ export default Profile = ({ navigation }) => {
 
 const styles = StyleSheet.create({
   image: {
-    height: 330,
-    width: 330,
-    marginTop: 30
+    height: 200,
+    width: 200,
+    marginTop: 50,
+    marginBottom: 20,
   },
   profileContainer: {
     flex: 1,
@@ -63,23 +119,23 @@ const styles = StyleSheet.create({
     fontSize: 30,
   },
   profileButton: {
-    backgroundColor: '#018744',
-         height: 54,
-         width: '80%',
-         borderRadius: 8,
-         marginBottom: 10,
-         alignItems: 'center',
-         justifyContent: 'center',
-         backgroundColor: '#0078AE',
-         shadowRadius: 5,
-         shadowOpacity: 0.7,
-         shadowColor: '#0078AE',
+    backgroundColor: "#018744",
+    height: 54,
+    width: "80%",
+    borderRadius: 8,
+    marginBottom: 10,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#0078AE",
+    shadowRadius: 5,
+    shadowOpacity: 0.7,
+    shadowColor: "#0078AE",
 
-         shadowOffset: {
-           width: 0,
-           height: 3,
-     },
+    shadowOffset: {
+      width: 0,
+      height: 3,
     },
+  },
   buttonText: {
     color: "white",
     alignItems: "center",
